a382236102ad9ee994213210def6ac29881cf26e
Mark van Eijk <mark@vaneijk.co>
2021-01-01 17:39:23 +0100

    wip

--- app/Commands/ImportDatabase.php
+++ app/Commands/ImportDatabase.php
@@ -13,10 +13,19 @@ class ImportDatabase extends Command
     public function handle()
     {
         $site = $this->argument('site');
-        $password = $this->ask('Please provide database password');
+        $password = $this->secret('Please provide database password');
 
-        $process = Process::fromShellCommandline("ssh rocketeer@{$site} 'mysqldump -u {$site} --password=\"{$password}\" {$site} | gzip' | gunzip | mysql -u root --password=\"\" {$site}");
+        // $process = Process::fromShellCommandline("ssh rocketeer@{$site} 'cd /var/www/".$site." && grep DB_PASSWORD .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)'");
 
-        $process->run(null, ['site' => $site, 'password' => $password]);
+        // dd('ssh rocketeer@{$site} "grep DB_PASSWORD /var/www/'.$site.'/.env | grep -v -e \'^\s*#\' | cut -d \'=\' -f 2-)"');
+        // echo $process->run();
+        // exit;
+
+        $process = Process::fromShellCommandline('mysql -u root --password="" -e "CREATE DATABASE IF NOT EXISTS '.$site.' CHARACTER SET utf8 COLLATE utf8_general_ci"');
+        echo $process->run();
+
+        $process = Process::fromShellCommandline("ssh rocketeer@{$site} 'sudo mysqldump -u {$site} --password=\"{$password}\" {$site} | sudo gzip' | gunzip | mysql -u root --password=\"\" {$site}");
+        dd("ssh rocketeer@{$site} 'sudo mysqldump -u {$site} --password=\"{$password}\" {$site} | sudo gzip' | gunzip | mysql -u root --password=\"\" {$site}");
+        echo $process->run(null, ['site' => $site, 'password' => $password]);
     }
 }
