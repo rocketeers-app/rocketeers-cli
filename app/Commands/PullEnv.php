1fc50889accec483590a3c6a210d7f78980f972b
Mark van Eijk <mark@vaneijk.co>
2021-01-11 21:39:47 +0100

    pull env server option

--- app/Commands/PullEnv.php
+++ app/Commands/PullEnv.php
@@ -7,14 +7,15 @@ use Spatie\Ssh\Ssh;
 
 class PullEnv extends Command
 {
-    protected $signature = 'env:pull {site}';
+    protected $signature = 'env:pull {site} {--server=}';
     protected $description = 'Pull env file';
 
     public function handle()
     {
         $site = $this->argument('site');
+        $server = $this->option('server') ?? $site;
 
-        $process = Ssh::create('rocketeer', $site)
+        $process = Ssh::create('rocketeer', $server)
             ->execute("cat /var/www/{$site}/.env");
 
         echo $process->getOutput();
