452d9827ab89fcf8722875003b8be719d3b12bcd
Mark van Eijk <mark@vaneijk.co>
2021-01-11 21:36:43 +0100

    db import with server option

--- app/Commands/ImportDatabase.php
+++ app/Commands/ImportDatabase.php
@@ -8,14 +8,15 @@ use Symfony\Component\Process\Process;
 
 class ImportDatabase extends Command
 {
-    protected $signature = 'db:import {site}';
+    protected $signature = 'db:import {site} {--server=}';
     protected $description = 'Import database';
 
     public function handle()
     {
         $site = $this->argument('site');
+        $server = $this->option('server') ?? $site;
 
-        $process = Ssh::create('rocketeer', $site)
+        $process = Ssh::create('rocketeer', $server)
             ->execute([
                 'grep DB_PASSWORD /var/www/'.$site."/.env | grep -v -e '^\s*#' | cut -d '=' -f 2-",
             ]);
@@ -25,7 +26,7 @@ class ImportDatabase extends Command
         $process = Process::fromShellCommandline("mysql -u root --password='' -e 'CREATE DATABASE IF NOT EXISTS `'".$site."'` CHARACTER SET utf8 COLLATE utf8_general_ci'");
         $process->run();
 
-        $process = Process::fromShellCommandline('ssh rocketeer@'.$site.' "sudo mysqldump --user=\''.$site.'\' --password=\''.$password.'\' --no-tablespaces \''.$site.'\' | sudo gzip" | gunzip | mysql -u root --password=\'\' \''.$site.'\'');
+        $process = Process::fromShellCommandline('ssh rocketeer@'.$server.' "sudo mysqldump --user=\''.$site.'\' --password=\''.$password.'\' --no-tablespaces \''.$site.'\' | sudo gzip" | gunzip | mysql -u root --password=\'\' \''.$site.'\'');
         $process->run();
     }
 }
