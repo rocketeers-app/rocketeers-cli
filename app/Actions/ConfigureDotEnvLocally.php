13d27685e000be1d3e412bf478a16aeebabb5cde
Mark van Eijk <mark@vaneijk.co>
2021-03-29 10:04:06 +0200

    configure app url to test domain

--- app/Actions/ConfigureDotEnvLocally.php
+++ app/Actions/ConfigureDotEnvLocally.php
@@ -10,13 +10,14 @@ class ConfigureDotEnvLocally
 
     public function handle($env, $name)
     {
-        $env = preg_replace('/^APP_ENV=(.*)/m', 'APP_ENV=local', $env);
         $env = preg_replace('/^APP_DEBUG=(.*)/m', 'APP_DEBUG=true', $env);
+        $env = preg_replace('/^APP_ENV=(.*)/m', 'APP_ENV=local', $env);
+        $env = preg_replace('/^APP_URL=(.*)/m', 'APP_URL=https://'.$name.'.test', $env);
         $env = preg_replace('/^CACHE_DRIVER=(.*)/m', 'CACHE_DRIVER=array', $env);
-        $env = preg_replace('/^DB_HOST=(.*)/m', 'DB_HOST=127.0.0.1', $env);
         $env = preg_replace('/^DB_DATABASE=(.*)/m', 'DB_DATABASE='.$name, $env);
-        $env = preg_replace('/^DB_USERNAME=(.+)/m', 'DB_USERNAME=root', $env);
+        $env = preg_replace('/^DB_HOST=(.*)/m', 'DB_HOST=127.0.0.1', $env);
         $env = preg_replace('/^DB_PASSWORD=(.*)/m', 'DB_PASSWORD=', $env);
+        $env = preg_replace('/^DB_USERNAME=(.+)/m', 'DB_USERNAME=root', $env);
 
         return $env;
     }
